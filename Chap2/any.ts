431981f39ae08dfb9f06ea38f74a1165d07852dc
joelmcdaniel <joelmcdbme@gmail.com>
2021-02-07 13:29:10 -0800

    first commit:
    - ch1 & ch2; review of typescript basics

---
+++ Chap2/any.ts
@@ -0,0 +1,6 @@
+/* let val: any = 22;
+val = "string value";
+val = new Array();
+// val.push(33);
+val.doesnotexist(33);
+console.log(val); */
---
+++ Chap2/literal.ts
@@ -0,0 +1,4 @@
+let literal: "tom" | "linda" | "jeff" | "sue" = "linda";
+literal = "sue";
+// literal = "john" // <- error
+console.log(literal)
